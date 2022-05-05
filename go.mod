d5ef33a5179e113f810786cb6a946ebce227b87e
nao215912 <naoki3.14159@gmail.com>
2022-05-05 14:38:57 +0900

    refactor

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module 42tokyo-road-to-dojo-go
 
 go 1.17
 
-require (
-	github.com/go-sql-driver/mysql v1.6.0
-	github.com/google/uuid v1.3.0
-)
+require github.com/go-sql-driver/mysql v1.6.0
--- pkg/database/object/user.go
+++ pkg/database/object/user.go
@@ -3,11 +3,11 @@ package object
 import "time"
 
 type User struct {
-	ID        string    `db:"id"`
-	Name      string    `db:"name"`
-	Token     string    `db:"token"`
-	HighScore int64     `db:"high_score"`
-	Coin      int64     `db:"coin"`
-	CreatedAt time.Time `db:"create_at"`
-	UpdatedAt time.Time `db:"updated_at"`
+	ID        string
+	Name      string
+	Token     string
+	HighScore int64
+	Coin      int64
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
