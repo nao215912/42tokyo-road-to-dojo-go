05c6472ed1e73725227a84f40ee4570ef2bbf068
nao215912 <naoki3.14159@gmail.com>
2022-05-05 16:43:19 +0900

    refactor

--- pkg/database/dao/struct_scan.go
+++ pkg/database/dao/struct_scan.go
@@ -7,10 +7,11 @@ import (
 
 func structScan(v interface{}, row *sql.Row) error {
 	rv := reflect.ValueOf(v).Elem()
-	values := make([]interface{}, rv.NumField())
+	n := rv.NumField()
+	values := make([]interface{}, n)
 	rv = reflect.Indirect(rv)
 
-	for i := 0; i < rv.NumField(); i++ {
+	for i := 0; i < n; i++ {
 		values[i] = rv.Field(i).Addr().Interface()
 	}
 	return row.Scan(values...)
